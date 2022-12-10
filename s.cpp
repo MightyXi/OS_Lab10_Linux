b2e9e0936fa946488fc52a1c5342457421e027c6
MightyXi <100283671+MightyXi@users.noreply.github.com>
2022-12-10 17:21:54 +0300

    Create s.cpp

---
+++ s.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+
+    string input;
+    getline(cin, input);
+    stringstream parcer(input);
+    stringstream output;
+    int curr_num;
+    long long sum = 0;
+    while(parcer >> curr_num){
+        sum += curr_num;
+    }
+    cout << sum;
+    return 0;
+}
